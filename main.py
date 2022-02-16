6cb262464e78f3263da9bc57f312b271ef88d0ba
Phoenix <g.maire118@gmail.com>
2022-02-17 00:45:42 +0100

    added auto crop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import json
+from collections import OrderedDict
 from PIL import Image
 from io import BytesIO
 from selenium import webdriver
@@ -17,6 +18,7 @@ def main():
     """Executes the different functions of the file"""
     # Initiates the webdriver
     options = Options()
+    options.add_argument("--kiosk")
     driver = webdriver.Chrome(options=options)
     # Go to the height comparator website
     driver.get('https://hikaku-sitatter.com/en/')
@@ -24,14 +26,17 @@ def main():
     with open('liste_taille.json', 'r') as f:
         names_dict = json.load(f)
     # Execute all the functions needed for each page of 10 people in the JSON
-    for i in range(len(names_dict)):
+    n = 0 # total amount of images
+    for i in range(int(len(names_dict)/10 + 1)):
         init_and_reset_graph(driver)
         add_10_names_from_json(driver, i)
         take_screenshot(driver, i)
+        n += 1
 
-    concatenate_images(driver)
-    time.sleep(1)
+    concatenate_images(n)
+    crop_white_spaces()
     driver.quit()
+
 
 def init_and_reset_graph(driver):
     """Adds someone whose height is 190cm to set the graph at a good scale. Then deletes all the default people
@@ -58,6 +63,7 @@ def init_and_reset_graph(driver):
     btn_del = driver.find_element_by_class_name('btn-del')
     driver.execute_script("arguments[0].click();", btn_del)
 
+
 def add_10_names_from_json(driver, page_number):
     """"Add 10 people to the website with correct name/height/gender, which is the limit."""
     # Find needed elements
@@ -65,24 +71,31 @@ def add_10_names_from_json(driver, page_number):
     height_textbox = driver.find_element_by_id('height')
     male_female_buttons = driver.find_elements_by_class_name('addInput-gender__label')
     send_button = driver.find_element_by_class_name('addInput__btn')
+
     # Loads JSON with names/height/gender, then add them into input fields and adds them one after another
     with open('liste_taille.json', 'r') as f:
-        names_dict = json.load(f)
-        # Add all the data into text fields then submits, for each person
-        for name, list_data in names_dict[f'{page_number}'].items():
-            name_textbox.send_keys(name)
-            height_textbox.send_keys(list_data[0])
-            if list_data[1] == 'm':
-                male_female_buttons[0].click()
-            else:
-                male_female_buttons[1].click()
-            send_button.click()
-            time.sleep(0.1)
+        names_dict = json.load(f, object_pairs_hook=OrderedDict)
+
+    # Add all the data into text fields then submits, for each person
+    for index, (name, list_data) in enumerate(names_dict.items()):
+        if index < 10 * page_number or index >= 10 * (page_number+1) :
+            continue
+
+        name_textbox.send_keys(name)
+        height_textbox.send_keys(list_data[0])
+
+        if list_data[1] == 'm':
+            male_female_buttons[0].click()
+        else:
+            male_female_buttons[1].click()
+        send_button.click()
+        time.sleep(0.1)
 
 # left 675
 # top 1264
 # right 1768
 # bottom 117
+
 
 def take_screenshot(driver, page_number):
     """Takes a screenshot of the frame and saves it"""
@@ -107,8 +120,9 @@ def take_screenshot(driver, page_number):
     im = im.crop((left, top, right, bottom))
     im.save(f'screenshot_{page_number}.png')
 
-def concatenate_images(driver):
-    images = [Image.open(x) for x in ['screenshot_0.png', 'screenshot_1.png']]
+
+def concatenate_images(n):
+    images = [Image.open(x) for x in [f"screenshot_{i}.png" for i in range(n)]]
     widths, heights = zip(*(i.size for i in images))
     total_width = sum(widths)
     max_height = max(heights)
@@ -118,8 +132,41 @@ def concatenate_images(driver):
         new_im.paste(im, (x_offset,0))
         x_offset += im.size[0]
 
-    new_im.save('final_image.png')
+    new_im.save('final.png')
+
+
+def _find_blank_cols(pixels, width, height):
+    blank_line = [pixels[0, y] for y in range(height)]
+    cols_found = []
+    for x in range(width):
+        col_x = [pixels[x, y] for y in range(height)]
+        if col_x == blank_line:
+            cols_found.append(x)
+    return cols_found
+
+
+def crop_white_spaces():
+    im = Image.open("final.png")
+    if im.mode != 'RGB':
+        im = im.convert('RGB')
+    pixels = im.load()
+    width, height = im.size[0], im.size[1]
+
+    cols_to_remove = _find_blank_cols(pixels, width, height)
+    new_im = Image.new('RGB', (width - len(cols_to_remove), height))
+    new_pixels = new_im.load()
+    
+    cols_removed = 0
+    for x in range(im.size[0]):
+        if x not in cols_to_remove:
+            for y in range(new_im.size[1]):
+                new_pixels[x - cols_removed, y] = pixels[x, y]
+        else:
+            cols_removed += 1
+    
+    new_im.save("final.png")
 
 
 if __name__ == '__main__':
+    # main()
     main()
